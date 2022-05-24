e0d171337138464dc6ecd18d238f9371eee6f66f
Bobby More <bobbymore2025@gmail.com>
2022-05-25 00:34:47 +0530

    slider addded

--- client/bookClient/src/app/books/books.component.ts
+++ client/bookClient/src/app/books/books.component.ts
@@ -10,6 +10,15 @@ import { BookServices } from './book.services';
 })
 
 export class BooksComponent implements OnInit {
+  imgSrc:string="	https://rukminim2.flixcart.com/flap/3600/3600/image/b3fe381767050079.jpg?q=80"
+  images:string[] = ["https://rukminim2.flixcart.com/flap/3600/3600/image/b3fe381767050079.jpg?q=80",
+  "https://rukminim2.flixcart.com/flap/3600/3600/image/539e571efc670481.jpg?q=80", 
+  "https://rukminim2.flixcart.com/flap/3600/3600/image/b6bc3ed0ef0b6fef.jpg?q=80", 
+  "https://rukminim2.flixcart.com/flap/3600/3600/image/fe161b55d4ef38c2.jpg?q=80", 
+  "https://rukminim2.flixcart.com/flap/3600/3600/image/0f727f5598c4b21a.jpg?q=80", 
+  "https://rukminim2.flixcart.com/flap/3600/3600/image/b463578cd69b58e4.png?q=80",
+  "https://rukminim2.flixcart.com/flap/3600/3600/image/7f5e1a6d7cf3e1eb.jpg?q=80",
+ ]
   allBooks: Book[] = [{
     addedOn: "",
     authors: "",
@@ -28,6 +37,7 @@ export class BooksComponent implements OnInit {
 
   ngOnInit(): void {
     this.getBooksData();
+    this.startSlideshow();
   }
 
   getBooksData() {
@@ -36,6 +46,23 @@ export class BooksComponent implements OnInit {
       this.allBooks=res.bookList;
 
     });
+<<<<<<< Updated upstream
+=======
+  }
+    startSlideshow() {
+      this.imgSrc = this.images[5];
+      let counter = 0;
+      setInterval(() => {
+          if (counter == this.images.length) {
+              counter = 0;
+          }
+          this.imgSrc = this.images[counter]
+          counter++;
+      }, 2000)
+  }
+  
+
+>>>>>>> Stashed changes
 
   }
-}
+
