eb17545caf77fb6bc42364118288a02cd90e667a
Bobby More <bobbymore2025@gmail.com>
2022-05-29 00:23:43 +0530

    update user details done  and some other changes

--- client/bookClient/src/app/profile/profile.component.ts
+++ client/bookClient/src/app/profile/profile.component.ts
@@ -6,13 +6,27 @@ import { HomeServices } from '../home/home.service';
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
+
 export class ProfileComponent implements OnInit {
+  editMode:boolean=true;
+  typeInput="password";
+  change="visibility_off"
+  type=false;
   id: string = ""
   name: string = ""
   email: string = ""
   mobile: number = 0
   address: string = ""
-
+  password:string="";
+   
+  tempUser={
+    id: "",
+    fullName: "",
+    email:"",
+    mobNumber:  0,
+    address: "",
+    password:"",
+  }
   constructor(private homeService: HomeServices) { }
 
   ngOnInit(): void {
@@ -32,6 +46,41 @@ export class ProfileComponent implements OnInit {
     this.email = obj.user.email;
     this.mobile = obj.user.mobNumber;
     this.address = obj.user.address;
+    this.password=obj.user.password;
   }
+   setEditMode(mode:boolean) {
+     this.editMode=mode;
+     if(mode==true){
+       this.tempUser.id=this.id;
+       this.tempUser.address=this.address;
+       this.tempUser.email=this.email;
+       this.tempUser.mobNumber=this.mobile;
+       this.tempUser.password=this.password;
+       this.tempUser.fullName=this.name;
+     }
+
+   }
+   updateUser(){
+    this.homeService.updateUser(this.tempUser).subscribe((res) => {
+      this.fetchUserData()
+      this.editMode = false;
+      alert(`The Book ${this.tempUser.fullName} with id ${this.tempUser.id} updated successfuly`);
+    });;
+ 
+   }
+
+   changeType(type:string){
+     if(type=="password"){
+      this.typeInput="text"
+      this.change="visibility"
+      
+    }
+   else{ 
+      this.typeInput="password"
+      this.change="visibility_off"
+   }
+    
+   }
+
 
 }
