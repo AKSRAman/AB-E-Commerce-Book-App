3182bdd2429f5a92c1cf36c9fdccb8091e484968
Aman Kumar <amankumarsingh60820@gmail.com>
2022-05-30 11:46:37 +0530

    Dummy Payment form added

--- client/bookClient/src/app/cart/cart.component.ts
+++ client/bookClient/src/app/cart/cart.component.ts
@@ -98,4 +98,20 @@ export class CartComponent implements OnInit {
     }
     this.totalPrice = total;
   }
+
+  simpleAlert() {
+    Swal.fire({
+      title: `Your order has been placed successfully`,
+      text: 'Thankyou',
+      timer: 1000,
+      icon: 'success',
+    });
+  }
+
+  isCheckOut = false
+
+  checkOutDone() {
+    this.isCheckOut ? this.isCheckOut = false : this.isCheckOut = true;
+  }
+
 }
